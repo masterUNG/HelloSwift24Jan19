670d81fe7bc5908ed91a9cef5124987e00711f8b
masterUNG <phrombutr@gmail.com>
2019-01-24 10:39:51 +0700

    Change Label Success

--- HelloSwift24Jan19/ViewController.swift
+++ HelloSwift24Jan19/ViewController.swift
@@ -9,12 +9,29 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+//    Explicit
+    let myName: String = "Doramon"
+    
+    
+    @IBOutlet weak var titleLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+    }   // Main Method
+    
+    
+    @IBAction func changeButton(_ sender: UIButton) {
+        
+//        Replace to myName
+        titleLabel.text = myName
+        
+        
     }
+    
+    
 
 
-}
+}   // Main Class
 
